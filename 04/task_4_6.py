38ed448dab253f244d486533ff361bd7f58a0138
Denisov Denis <d.l.denisov@gmail.com>
2019-12-18 17:52:34 +0300

    Task 4.6 done

--- 04/task_4_6.py
+++ 04/task_4_6.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 Задание 4.6
@@ -15,3 +16,17 @@ Outbound Interface:    FastEthernet0/0
 '''
 
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+
+rt = ospf_route.split()
+
+output ="""\
+Protocol:              {}
+Prefix:                {}
+AD/Metric:             {}
+Next-Hop:              {}
+Last update:           {}
+Outbound Interface:    {}
+"""
+
+
+print(output.format(rt[0]+'SPF', rt[1], rt[2].strip('[]'), rt[4].rstrip(','), rt[5].rstrip(','), rt[6]))
