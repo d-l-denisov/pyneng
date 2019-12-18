5be9c78d5a26e662bdedc42afe5f1ef15366016b
Denisov Denis <d.l.denisov@gmail.com>
2019-12-18 17:20:10 +0300

    Task 4.5 done

--- 04/task_4_5.py
+++ 04/task_4_5.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 Задание 4.5
@@ -13,3 +14,5 @@
 
 command1 = 'switchport trunk allowed vlan 1,2,3,5,8'
 command2 = 'switchport trunk allowed vlan 1,3,8,9'
+
+print(list(set(command1.split()[-1].split(',')).intersection(set(command2.split()[-1].split(',')))))
