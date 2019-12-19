58bc858136f5f52df1cdfef56d7deabd267f36d2
Denisov Denis <d.l.denisov@gmail.com>
2019-12-19 16:05:00 +0300

    Task 5.2 done

--- 05/task_5_2.py
+++ 05/task_5_2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 Задание 5.2
@@ -19,3 +20,39 @@ Mask:
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+net = input("Enter IP network (net/mask): ")
+
+net = net.split('.')
+net.extend(net.pop().split('/'))
+
+print('Network:')
+
+print('{:10}'.format(net[0]),
+      '{:10}'.format(net[1]),
+      '{:10}'.format(net[2]),
+      '{:10}'.format(net[3]),
+      '\n',
+      '{:08b}'.format(int(net[0])), '  ',
+      '{:08b}'.format(int(net[1])), '  ',
+      '{:08b}'.format(int(net[2])), '  ',
+      '{:08b}'.format(int(net[3])), '\n',
+     sep='')
+
+print('Mask:')
+
+print('/', net[4], sep = '')
+
+mask = '1' * int(net[4]) + '0' * (32 - int(net[4]))
+print('{:<10}'.format(int(mask[0:8], 2)),
+      '{:<10}'.format(int(mask[8:16], 2)),
+      '{:<10}'.format(int(mask[16:24], 2)),
+      '{:<10}'.format(int(mask[24:], 2)),
+      sep = '')
+
+print('{:08b}'.format(int(mask[0:8], 2)), '  ',
+      '{:08b}'.format(int(mask[8:16], 2)), '  ',
+      '{:08b}'.format(int(mask[16:24], 2)), '  ',
+      '{:08b}'.format(int(mask[24:], 2)), '  ',
+      sep = '')
+
