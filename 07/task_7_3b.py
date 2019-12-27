13cf540af15df204ce7ae0686ac8866237ab72f7
Denisov Denis <d.l.denisov@gmail.com>
2019-12-27 15:50:12 +0300

    Task 7.3b done

--- 07/task_7_3b.py
+++ 07/task_7_3b.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 Задание 7.3b
@@ -11,3 +12,18 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+
+vlan = input('Enter VLAN number: ')
+
+with open('./CAM_table.txt') as f:
+    for line in f:
+        line = line.split()
+        if line and line[0].isdigit():
+            line[0] = int(line[0])
+            if line[0] == int(vlan):
+                print('{:<9}'.format(line[0]),
+                     '{:17}'.format(line[1]),
+                     '{:5}'.format(line[3]),
+                     sep = ''
+                     )
+
