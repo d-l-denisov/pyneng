05c03f803589355323373aa0fcd24b7df36ebfd2
Denis Denisov <d.l.denisov@gmail.com>
2020-01-01 16:28:23 +0300

    Task 9.3 done

--- 09/task_9_3.py
+++ 09/task_9_3.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 Задание 9.3
@@ -21,4 +22,31 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+def get_int_vlan_map(config_filename):
+    '''
+    Function receives configuration filename (config_filename), parses it and returns
+    tuple that contain 2 dictionaries (accsess port: vlan and trunk port: vlan list)
+    '''
+    
+    with open(config_filename) as f:
+        intf_list = []
+        vlan_list = []
+        for line in f:
+            line = line.strip()
+            if line.startswith('interface'):
+                intf_list.append(line.split()[-1].rstrip())
+                vlan_list.append(None)
+            if 'switchport access vlan' in line:
+                vlan_list[-1] = int(line.split()[-1])
+            elif 'switchport trunk allowed vlan' in line:
+                vlan_list[-1] = [int(vlan) for vlan in line.split()[-1].split(',')]
+
+        return (dict(e for e in zip(intf_list, vlan_list) if e[1] and type(e[1]) == int),
+                dict(e for e in zip(intf_list, vlan_list) if e[1] and type(e[1]) == list)) 
+        
+
+from sys import argv
+
+print(get_int_vlan_map(argv[1]))
 
