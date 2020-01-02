aff3adf4592c449fa585676945de78c689a7fa7b
Denis Denisov <d.l.denisov@gmail.com>
2020-01-02 11:03:00 +0300

    Test 9.4 done

--- 09/task_9_4.py
+++ 09/task_9_4.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 Задание 9.4
@@ -35,3 +36,31 @@ def ignore_command(command, ignore):
     * False - если нет
     '''
     return any(word in command for word in ignore)
+
+
+
+def convert_config_to_dict(config_filename):
+    '''
+    Function receives config filename, reads and parses it. It returns dictionary with upper level
+    commands as a key and list of lower level commands as a value (or empty list if no lower commands)
+    '''
+    d_res = {}
+
+    with open(config_filename) as f:
+        for line in f:
+            if not (line.startswith('!') or ignore_command(line, ignore) or not line.strip()):
+                if line.startswith(' '):
+                    d_res[upper_level_command].append(line.strip())
+                else:
+                    line = line.strip()
+                    d_res[line] = []
+                    upper_level_command = line
+    return d_res
+
+
+
+from sys import argv
+
+print(convert_config_to_dict(argv[1]))
+
+
