d258cbe01d6ebc41a72ef999bc0e5a4dcc4bc94d
Denisov Denis <d.l.denisov@gmail.com>
2020-01-09 15:54:07 +0300

    Task 11.1 done

--- 11/task_11_1.py
+++ 11/task_11_1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 Задание 11.1
@@ -27,4 +28,40 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+
+def parse_cdp_neighbors(command_output):
+    """
+    Function receives output of show cdp neighbor command as single line string,
+    parses it and returns dictionary that describes devices interconnection
+
+    Example:
+
+    R4>show cdp neighbors
+    Device ID    Local Intrfce   Holdtme     Capability       Platform    Port ID
+    R5           Fa 0/1          122           R S I           2811       Fa 0/1
+    R6           Fa 0/2          143           R S I           2811       Fa 0/0
+
+    Function return:
+
+        {('R4', 'Fa0/1'): ('R5', 'Fa0/1'),
+         ('R4', 'Fa0/2'): ('R6', 'Fa0/0')}
+
+    """
+    dev_a = command_output[:command_output.find('>')].strip()
+    lines = [ l for l in command_output[command_output.find('Device ID'):].split('\n')[1:] if l ]
+    d_res = {}
+    for line in lines:
+        dev_b, dev_a_intf_type, dev_a_intf_num, *_, dev_b_intf_type, dev_b_intf_num = line.split()
+        d_res[(dev_a, dev_a_intf_type + dev_a_intf_num)] = (dev_b, dev_b_intf_type + dev_b_intf_num)
+    return d_res
+
+
+from sys import argv
+
+
+with open(argv[1]) as f:
+    cdp_output = f.read()
+    
+    print(parse_cdp_neighbors(cdp_output))
 
