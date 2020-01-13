c7bcbd3cde5369446bb70f5bf4d57cfb822ca920
Denisov Denis <d.l.denisov@gmail.com>
2020-01-13 15:08:03 +0300

    Task 12.1 done

--- 12/task_12_1.py
+++ 12/task_12_1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 Задание 12.1
@@ -15,3 +16,27 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
 
+from sys import argv
+import subprocess
+
+
+def ping_ip_addresses(ip_list):
+    """
+    Function receives list of IP addresses and returns tuple with 2 list-elements: pingable IP
+    and non-pingable IP addresses
+    """
+
+    t_res = ([], [])
+    for ip in ip_list:
+        sp_res= subprocess.run(['ping', '-c1', '-n', ip],
+                       stdout = subprocess.DEVNULL,
+                       stderr = subprocess.DEVNULL)
+        if sp_res.returncode == 0:
+            t_res[0].append(ip)
+        else:
+            t_res[1].append(ip)
+    return t_res
+
+
+if __name__ == '__main__':
+    print('\n',ping_ip_addresses(argv[1:]))
