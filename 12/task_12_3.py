e49cda915c5a5a3deddd60ab620fbcc4942fec42
Denisov Denis <d.l.denisov@gmail.com>
2020-01-14 17:42:34 +0300

    Task 12.3 done

--- 12/task_12_3.py
+++ 12/task_12_3.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 Задание 12.3
@@ -23,3 +24,17 @@ Reachable    Unreachable
 
 Для этого задания нет тестов
 '''
+
+from tabulate import tabulate
+from itertools import zip_longest
+
+def print_ip_table(reachable, unreachable):
+    print(tabulate(zip_longest(reachable, unreachable, fillvalue = ''),
+                  headers=['Reachable', 'Unreachable']))
+
+reachable = ['10.1.1.1', '10.1.1.2']
+unreachable = ['10.1.1.7', '10.1.1.8', '10.1.1.9']
+
+if __name__ == '__main__':
+    print_ip_table(reachable, unreachable)
+
