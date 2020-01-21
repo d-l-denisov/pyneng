0d9dd6935a3ae2cb286d4b138035d64f71319497
Denisov Denis <d.l.denisov@gmail.com>
2020-01-21 13:58:21 +0300

    Task 15.2 done

--- 15/task_15_2.py
+++ 15/task_15_2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 Задание 15.2
@@ -21,3 +22,18 @@
 Проверить работу функции на примере файла sh_ip_int_br.txt.
 
 '''
+
+from sys import argv
+from pprint import pprint
+import re
+
+def parse_sh_ip_int_br(filename):
+    result = []
+    with open(filename) as f:
+        lines = f.read()
+        match = re.finditer(r'(\S+) +(\S+) .+ (up|down|aministratively down) +(up|down)', lines)
+        result = [m.groups() for m in match]
+    return result
+
+if __name__ == '__main__':
+    pprint(parse_sh_ip_int_br(argv[1]))
